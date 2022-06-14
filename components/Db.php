b935e7da5612fd10d951068724cb2a10f362bb15
Shizuco <93375450+Shizuco@users.noreply.github.com>
2022-06-14 07:40:02 +0300

    Update Db.php

--- components/Db.php
+++ components/Db.php
@@ -8,8 +8,7 @@ class Db{
     public static function getConnected(){
         $paramsPath = ROOT . '/config/db_params.php';
         $params = include($paramsPath);
-        $dsn = "mysql:host={$params['host']};port=3306;dbname={$params['dbname']}";
-        $db = new PDO($dsn, $params['user'], $params['password']);
+        $db = new PDO($params['inst'], $params['user'], $params['password']);
         return $db;
     }
 }
